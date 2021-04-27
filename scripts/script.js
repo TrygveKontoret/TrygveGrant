8678fc9b6e0caef56dc751d98aa1273696b84166
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-04-27 18:11:19 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,6 @@
 const url = "https://trygvegrant.no/wp-json/wp/v2/posts?_embed=true";
 const out = document.querySelector(".blog");
 
-// blob = (blogs) => {
-//     console.log(blogs);
-//     out.innerHTML = "";
-//     for (blog of blogs) {
-//         console.log(blog)
-//         let nyBlog = `
-//         <div>
-//         <h2>${blog.title.rendered}</h2>
-//         <p>${blog.content.rendered}</p>
-//         <p>${blog._embedded}</p>
-//         </div>
-//         `;
-//         out.innerHTML += nyBlog;
-//     }
-// }
 
 fetch(url)
     .then(response => response.json())
