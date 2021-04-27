5622cf20917a7ce2b945be938d77fb049cb865a8
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-04-27 16:51:29 +0200

    FIkk bildet til å funke

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
-const url = "https://trygvegrant.no/wp-json/wp/v2/posts/?_embed";
+const url = "https://trygvegrant.no/wp-json/wp/v2/posts?_embed=true";
 const out = document.querySelector(".blog");
 
-blob = (blogs) => {
-    console.log(blogs);
-    out.innerHTML = "";
-    for (blog of blogs) {
-        console.log(blog)
-        let nyBlog = `
-        <div>
-        <h2>${blog.title.rendered}</h2>
-        <p>${blog.content.rendered}</p>
-        <p>${blog.featured_media}</p>
-        </div>
-        `;
-        out.innerHTML += nyBlog;
-    }
-}
+// blob = (blogs) => {
+//     console.log(blogs);
+//     out.innerHTML = "";
+//     for (blog of blogs) {
+//         console.log(blog)
+//         let nyBlog = `
+//         <div>
+//         <h2>${blog.title.rendered}</h2>
+//         <p>${blog.content.rendered}</p>
+//         <p>${blog._embedded}</p>
+//         </div>
+//         `;
+//         out.innerHTML += nyBlog;
+//     }
+// }
 
 fetch(url)
     .then(response => response.json())
@@ -23,5 +23,25 @@ fetch(url)
     .catch(error => {
         console.error(error);
         out.innerHTML = "Something went wrong..."
-    })
+    }
+    )
 
+    blob = (blogs) => {
+        console.log(blogs);
+            for (blog of blogs) {
+                let media = blog._embedded["wp:featuredmedia"]
+                for (imgs of media){
+                    console.log(imgs);
+                    let nyBlog = `
+                    <div>
+                    <h2>${blog.title.rendered}</h2>
+                    <p>${blog.content.rendered}</p>
+                    <img src="${imgs.source_url}" alt="YOYO">
+                    </div>
+                    `;
+                    out.innerHTML += nyBlog;
+                }
+        }
+    }
+
+    
