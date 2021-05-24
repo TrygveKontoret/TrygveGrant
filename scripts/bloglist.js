24a512c7003309b2e0eb5299cfab7c7a079dd230
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-05-24 15:46:37 +0200

    view more

--- scripts/bloglist.js
+++ scripts/bloglist.js
@@ -1,6 +1,8 @@
-const url = "https://trygvegrant.no/wp-json/wp/v2/posts?_embed=true&per_page=100";
+const url = "https://trygvegrant.no/wp-json/wp/v2/posts?_embed=true&per_page=10";
+const urlMore = "https://trygvegrant.no/wp-json/wp/v2/posts?_embed=true&per_page=2&offset=10"
 const out = document.querySelector(".blogPosts");
 const loading = document.querySelector(".loading");
+const viewMore = document.querySelector(".more");
 
 
 fetch(url)
@@ -31,3 +33,13 @@ blob = (blogs) => {
             }
     }
 }
+
+viewMore.addEventListener("click", ()=> {
+    fetch(urlMore)
+    .then(response => response.json())
+    .then(data => blob(data))
+    .catch(error => {
+        console.error(error);
+        out.innerHTML = "Something went wrong..."})
+    .finally(()=> loading.style.display="none", viewMore.style.display="none");
+})
