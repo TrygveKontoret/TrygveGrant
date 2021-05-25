446a747006cb78507bfdd9eb881c7ae11d23235c
Trygve Grant <69842793+TrygveKontoret@users.noreply.github.com>
2021-05-25 09:40:20 +0200

    .

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,6 +1,7 @@
-const url = "https://trygvegrant.no/wp-json/wp/v2/posts?_embed=true&per_page=100";
-const out = document.querySelector(".carousel");
+const url = "https://trygvegrant.no/wp-json/wp/v2/posts?_embed=true&per_page=8";
+const out = document.querySelector(".carousel-slide");
 const loading = document.querySelector(".loading");
+const carouselDiv = document.querySelector(".card");
 
 
 fetch(url)
@@ -19,7 +20,7 @@ blogPosts = (blogs) => {
             for (imgs of media){
                 console.log(imgs);
                 let nyBlog = `
-                <div>
+                <div class="card">
                     <a href="/details.html?id=${blog.id}"><h2>${blog.title.rendered}</h2></a>
                     <div class="imgContainer">
                     <a href="/details.html?id=${blog.id}"><img src="${imgs.media_details.sizes.medium.source_url}" alt="Movie poster"></a>
@@ -30,3 +31,36 @@ blogPosts = (blogs) => {
             }
     }
 }
+
+
+// buttons
+
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+// counter
+
+let counter = 1;
+const size = carouselDiv;
+
+out.style.transform = 'translateX(' + (counter) + 'px)';
+
+// Button listener
+
+nextBtn.addEventListener('click', ()=>{
+    out.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    console.log(counter);
+    out.style.transform = 'translateX(' + (counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', ()=>{
+    out.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    console.log(counter);
+    out.style.transform = 'translateX(' + (counter) + 'px)';
+});
+
+out.addEventListener('transitionend', ()=>{
+    console.log('Fired');
+});
